38e46047744b46068ef1abeb2e54523c9f5cc0dc
Taleb01 <taleb_n1@hotmail.com>
2017-10-24 16:20:57 +0200

    last Uppdate

--- Exercise3/PersonHandler.cs
+++ Exercise3/PersonHandler.cs
@@ -25,5 +25,85 @@ namespace Exercise3
             hello.Height = height;
             return hello; 
         }
+        public bool MainMenu()
+        {
+            bool KeepGoing = true;
+            while (KeepGoing)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcom to Details!");
+                Console.WriteLine("1)Enter Details ");
+                Console.WriteLine("2) Exit.....Bye! ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+
+                    case "1":
+                        PersonInformation();
+                        break;
+                    case "2":
+                        KeepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Bye....");
+                        Console.ReadLine();
+                        break;
+
+                }
+                
+            }
+            return true;
+        }
+        public static void PersonInformation()
+        {
+            string input;
+            string fName;
+            string lName;
+            double height;
+            double weight;
+            int age = 0;
+            bool correctAge = true;
+            string heightt;
+            string weightt;
+            do
+            {
+                if (!correctAge)
+                {
+                    Console.WriteLine("Please Try Again \n");
+                }
+                Console.Clear();
+                Console.Write("Welcome! ");
+                input = Console.ReadLine();
+                correctAge = int.TryParse(input, out age);
+            }
+            while (correctAge);
+            if (age >= 0)
+            {
+                Console.WriteLine("what is your age:");
+                age = int.Parse(Console.ReadLine());
+                Console.Write("What is your first name? ");
+                fName = Console.ReadLine();
+                Console.Write("What is your last name? ");
+                lName = Console.ReadLine();
+                Console.Write("What's your height?: ");
+                heightt = Console.ReadLine();
+                double.TryParse(heightt, out height);
+                Console.Write("What's your weight? ");
+                weightt = Console.ReadLine();
+                double.TryParse(weightt, out weight);
+
+
+                Console.WriteLine("Hello " + fName + " " + lName + " " + " yrou are: " + age + " years old " + " with " + height + " and " + weightt + " weight");
+                Console.ReadLine();
+            }
+
+            else if (age < 0)
+            {
+                Console.WriteLine("Please try Again\n and Inseart a valid age... ");
+                Console.ReadLine();
+            }
+
+        }
     }
 }
