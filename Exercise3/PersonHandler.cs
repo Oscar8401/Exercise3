df14eef64a57104d3d0e05e1a31066e96aa79c78
Taleb01 <taleb_n1@hotmail.com>
2017-10-24 13:52:19 +0200

    Upddates

---
+++ Exercise3/PersonHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class PersonHandler
+    {
+        public void SetAge (Person pers, int age)
+        {
+            pers.Age = age;
+        }
+
+        public Person CreatePerson (int age, string fname, string lname, double height, double weight)
+        {
+
+
+            Person hello = new Person();
+            hello.Age = age;
+            hello.FName = fname;
+            hello.LName = lname;
+            hello.Weight = weight;
+            hello.Height = height;
+            return hello;
+        }
+    }
+}
